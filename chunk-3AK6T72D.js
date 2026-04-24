34ba4423aae3a4d58cf8119bc1095aef72191f31
emamirelar <emamirelar@unops.org>
2026-04-24 11:39:21 +0200

    Auto-generated commit

---
+++ chunk-3AK6T72D.js
@@ -0,0 +1,1 @@
+import"./chunk-DAQOROHW.js";var a=[{path:"",redirectTo:"inbox",pathMatch:"full"},{path:"inbox",loadComponent:()=>import("./chunk-4LXK6SNB.js").then(t=>t.MailInbox),data:{breadcrumb:"Inbox"}},{path:"detail/:id",loadComponent:()=>import("./chunk-IIW5WAJE.js").then(t=>t.MailDetail),data:{breadcrumb:"Detail"}}];export{a as default};
